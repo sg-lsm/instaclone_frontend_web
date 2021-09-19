4875bce54df5360329261cc24acfd7cc9deebb48
RIPPI <ckdgmlrt@gmail.com>
2021-09-20 00:23:31 +0900

    initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router";
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <h1>hello</h1>
+          </Route>
+          <Route path="/papaya">
+            <h1>papaya</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
